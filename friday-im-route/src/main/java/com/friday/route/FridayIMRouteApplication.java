df5692785f3edb3a0ab73eec584afce5f42957da
Damon <554588461@qq.com>
2020-06-03 10:22:12 +0800

    eurake加载成功

--- friday-im-route/src/main/java/com/friday/route/FridayIMRouteApplication.java
+++ friday-im-route/src/main/java/com/friday/route/FridayIMRouteApplication.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Copyright (C),Damon
@@ -17,7 +17,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @Slf4j
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class FridayIMRouteApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(FridayIMRouteApplication.class, args);
